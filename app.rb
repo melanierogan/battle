6bfeab59c3483b2d75e7fae3d64cbfa45f73b573
Melanie Rogan <melanie.rogan@ft.com>
2019-05-29 16:12:35 +0100

    Entering Players step done...now thinking about how things look

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Application
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @Player_1 = params[:Player_1]
+    @Player_2 = params[:Player_2]
+    erb(:play)
   end
 end
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -7,5 +7,13 @@ describe MyApp do
       visit '/'
       expect(page).to have_content 'Testing infrastructure working!'
     end
+
+    it 'should display names entered in a form' do
+      visit '/'
+      fill_in('Player_1', with: 'Ed')
+      fill_in('Player_2', with: 'Mel')
+      click_on 'Submit'
+      expect(page).to have_content('Player 1: Ed, Player 2: Mel')
+    end
   end
 end
