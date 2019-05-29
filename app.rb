845daaaeea52ed7ba5baf1fa5786420a068f6df1
Melanie Rogan <melanie.rogan@ft.com>
2019-05-29 17:37:07 +0100

    the tests pass but no params printing on the player page...bloody sessions

--- app.rb
+++ app.rb
@@ -2,21 +2,25 @@ require 'sinatra/base'
 
 
 class MyApp < Sinatra::Application
-  enable :sessions
+
+  configure do
+    enable :sessions
+  end
+  
   
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    session[:players] = [params[:Player_1], params[:Player_2]]
-    # erb(:play)
+    session[:Player_1] = params[:Player_1]
+    session[:Player_2] = params[:Player_2]
     redirect '/play'
   end
 
   get '/play' do
-    @Player_1 = session[:players][0]
-    @Player_2 = session[:players][1]
+    @Player_1 = session[:Player_1]
+    @Player_2 = session[:Player_2]
     erb :play
   end
 end
